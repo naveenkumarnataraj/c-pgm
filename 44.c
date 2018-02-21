563148ecd035127e532b6aae09a32ce8bfdb5607
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-21 14:49:27 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+main()
+{
+int a;
+    printf("enter the number\n");
+    scanf("%d",&a);
+    if(a<=10)
+    {
+            printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    
+    return 0;
+}
