6d5449cd821fe2c846047d172c52b757c3348218
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-21 15:07:56 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+main()
+{
+int a,c,b;
+    printf("enter the numeric digit\n\n");
+    scanf("%d%d",&a,&b);
+    
+    c=a+b;
+            printf("%d\n",c);
+    
+    return 0;
+}
+
+
