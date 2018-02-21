9e85cad27008f90cf400848dad94646f3995392f
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-21 14:40:38 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+main()
+{
+
+    char b[20],c[20];
+    printf("enter the string 1\n");
+    scanf("%s",&b);
+    printf("enter the string 2\n");
+    scanf("%s",&c);
+    
+        printf("%s%s",b,c);
+    
+    return 0;
+}
+
