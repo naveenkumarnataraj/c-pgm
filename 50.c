14932b7214eb5b1e28eb84d003f94e408bcd31a2
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-23 16:09:56 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	 if(n%2==0)
+	 {
+	 printf("the num is power of two");
+}
+else 
+{
+    printf(" the num is not power of two");
+}
+return 0;
+}
