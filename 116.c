6bfb6144159a800a0aedb5fddcbf778fe8de0ba3
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-28 15:02:42 +0530

    Create 116.c

---
+++ 116.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    printf("enter the value:");
+    scanf("%d",&n);
+    printf("enter the value");
+    scanf("%d",&m);
+    printf("the joint of given two num is:%d%d ",n,m);
+
+    return 0;
+}
+
