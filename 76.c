fd265bcc7ee3ae132453b9f4952839d2602307cb
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-03-05 14:11:00 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter the value:");
+   scanf("%d",&n);
+   if(n/10==0)
+{
+    printf("no");
+    
+}
+    else
+    {
+        printf("yes");
+    }
+    return 0;
+}
+
