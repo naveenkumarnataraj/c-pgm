dcf6cc8621ab09c058447992fb9bb51515bc9830
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-03-05 13:38:27 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int p;
+    printf("enter the value:");
+    scanf("%d",&p);
+   
+       if(p/10==0)
+       {
+           printf("the given was not composite number");
+       }
+       else
+ {  
+    printf(" the given is composite number ");
+}
+    return 0;
+}
+
+
