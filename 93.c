1d53a35e2c0f40a4beb4b3a774b0ae32a91c017e
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-03-05 13:22:37 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int celcius,kelvin;
+    printf("enter the temperature in celcius:");
+    scanf("%d",&celcius);
+    kelvin=celcius+273;
+    printf(" the given temperature in kelvin is:%d",kelvin);
+
+    return 0;
+}
+
+
