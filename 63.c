7e4d3e3e216d1de05c1b3658dfed43fab8e23e4c
NAVEENKUMAR <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-03-09 16:40:41 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(void)
+{
+	char s[50];
+	int i,c=1;
+	printf("enter the words:");
+	scanf("%s",&s);
+	for(i=0;s!='\0';i++);
+	{
+	if(s=='')
+	{
+	    
+	}
+	c=c+1;
+}	
+	printf("\n%d",c);
+	
+	return 0;
+}
